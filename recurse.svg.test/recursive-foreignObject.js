57156f2d0473db5a113cee0de12f0f0bcf8f678b
Rennie deGraaf <rennie.degraaf@gmail.com>
2014-07-27 23:48:18 -0700

    Recursion tests

---
+++ recurse.svg.test/recursive-foreignObject.js
@@ -0,0 +1,56 @@
+var http = require('http');
+
+var svg = '<?xml version="1.0" encoding="UTF-8" standalone="no"?>\
+<svg\
+   xmlns="http://www.w3.org/2000/svg"\
+   xmlns:xhtml="http://www.w3.org/1999/xhtml"\
+   width="68"\
+   height="68"\
+   viewBox="-34 -34 68 68"\
+   version="1.1">\
+  <circle\
+     cx="0"\
+     cy="0"\
+     r="24"\
+     fill="#c8c8c8"/>\
+  <foreignObject x="0" y="0" width="34" height="34">\
+    <xhtml:xhtml>\
+      <xhtml:head>\
+        <xhtml:style>\
+          document,body,img {\
+            padding: 0px;\
+            margin: 0px;\
+            border: 0px;\
+          }\
+        </xhtml:style>\
+        <xhtml:script>\
+       </xhtml:script>\
+      </xhtml:head>\
+      <xhtml:body>\
+        <xhtml:object width="34" height="34" type="image/svg+xml" data="REPLACE">circle</xhtml:object>\
+      </xhtml:body>\
+    </xhtml:xhtml>\
+  </foreignObject>\
+</svg> '
+
+http.createServer(function(request, response) {
+    var num = parseInt(request.url.substr(1))
+    if (isNaN(num) && "GET" == request.method) {
+        response.writeHead(400, {'Content-Type': 'text/plain'});
+        if ("GET" == request.method) {
+            console.log("Unrecognized request for " + request.url);
+            response.end("The request should be an integer.  Try '0'.");
+        } else {
+            response.end();
+        }
+    } else {
+        response.writeHead(200, {'Content-Type': 'image/svg+xml'});
+        if ("GET" == request.method) {
+            console.log(num);
+            response.end(svg.replace("REPLACE", ""+(num+1)));
+        } else {
+            response.end();
+        }
+    }
+}).listen(8001);
+
