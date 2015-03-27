b33645fb213af5cb7e2918eb9c67be2b0f488d0c
Rennie deGraaf <rennie.degraaf@gmail.com>
2015-03-26 21:47:27 -0700

    Added licensing information.

--- svg.test/funcs.php
+++ svg.test/funcs.php
@@ -1,3 +1,17 @@
+<?php /*
+  This file is part of the SVG Security Test Suite (SSTS).
+
+  SSTS is free software: you can redistribute it and/or modify it under the 
+  terms of version 2 of of the GNU General Public License as published by the 
+  Free Software Foundation.
+
+  Foobar is distributed in the hope that it will be useful, but WITHOUT ANY 
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR 
+  A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along with 
+  SSTS.  If not, see <https://www.gnu.org/licenses/gpl-2.0.html>.
+*/ ?>
 <?php 
 function getFile($url)
 {
